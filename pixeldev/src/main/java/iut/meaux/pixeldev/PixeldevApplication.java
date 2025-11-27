acf6e4dfac7721928ffeaddde2c7a84c2f1e8b81
leo.hm5 <leonardodacruzmateus@gmail.com>
2025-11-27 17:31:25 +0100

    Init

---
+++ pixeldev/src/main/java/iut/meaux/pixeldev/PixeldevApplication.java
@@ -0,0 +1,13 @@
+package iut.meaux.pixeldev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixeldevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixeldevApplication.class, args);
+	}
+
+}
